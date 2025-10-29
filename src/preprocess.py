124db302545d05fdfd0c3ddb36456acbac21aad8
Karen Xu <03karenxu@gmail.com>
2025-10-28 19:35:37 -0700

    file org and preprocessing update

---
+++ src/preprocess.py
@@ -0,0 +1,99 @@
+# PREPROCESS.PY -----------------------------------
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import SMOTE
+from scipy.interpolate import UnivariateSpline
+from sklearn.model_selection import train_test_split
+
+
+# main preprocess function
+def preprocess(x):
+    x = min_max_scale(x)
+    x = fit_spline(x)
+    return x
+
+
+# plots a random sample
+def plot_random_sample(x, y):
+    i = np.random.choice(x.shape[0])
+    row = x[i]
+    label = y[i]
+    plot_sample(row,label)
+
+
+# plots a specified sample
+def plot_sample(x,y):
+    plt.plot(x)
+    plt.xlabel("Time Step")
+    plt.ylabel("Router Activity")
+    plt.title(f"Label: {y}")
+    plt.show()
+    plt.close()
+
+
+# plots all samples overlayed
+def plot_all(label, x, y):
+    indices_label = np.where(y == label)[0]
+    plt.figure(figsize=(10, 6))
+    for i in indices_label:
+        plt.plot(x[i], alpha=0.3, color="blue")
+    plt.ylabel("Activity")
+    plt.title(f"All Samples with Label {label}")
+    plt.show()
+    plt.close()
+
+
+# loads data from csv and splits into train/test
+def load_data(file_path,test_ratio):
+    df = pd.read_csv(file_path, header=None)
+    x,y = split_features(df)
+    return train_test_split(
+        x, y, test_size=test_ratio, stratify=y
+    )
+
+
+# splits features from labels
+def split_features(df):
+    x = df.iloc[:, :-1].values.astype(float)
+    y = df.iloc[:, -1].values.astype(int)
+    return x,y
+
+
+# scales sample-wise to values between 0 and 1
+def min_max_scale(x):
+    x_min = x.min(axis=0)
+    x_max = x.max(axis=0)
+    return (x - x_min) / (x_max - x_min + 1e-9)
+
+
+# applies smote to minority class
+def apply_smote(x, y):
+    smote = SMOTE(sampling_strategy="minority")
+    x_res, y_res = smote.fit_resample(x, y)
+    return x_res, y_res
+
+
+# fits a univariate spline to all samples
+def fit_spline(x):
+    x_smoothed = []
+    x = np.array(x)
+    time_steps = np.arange(x.shape[1])
+    
+    for sample in x:
+        spl = UnivariateSpline(time_steps, sample,s=0.5)
+        smoothed_sample = spl(time_steps)
+        x_smoothed.append(smoothed_sample)
+    
+    return np.array(x_smoothed)
+
+
+# prints processed data to csv
+def save_processed_data(x, y, out_file):
+    processed_df = pd.DataFrame(x)
+    processed_df["label"] = y.astype(int)
+    processed_df.to_csv(out_file, index=False, header=False)
+
+
+# ------------------------------------------------------
